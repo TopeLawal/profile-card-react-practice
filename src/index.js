1be38ff519a659f46228451e66379a486150d777
obinna <temitopelaw1@gmail.com>
2025-06-09 13:20:51 +0100

    worked on a profile card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client";
+import React from "react";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <Main />
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img src="https://8szvtk.csb.app/jonas.jpeg" />;
+}
+
+function Main() {
+  return (
+    <main>
+      <h1>Jonas Schmedtmann</h1>
+      <p>
+        Full-stack web developer and teacher at Udemy. When not coding or
+        preparing a course, I like to play board games, to cook (and eat), or to
+        just enjoy the Portuguese sun at the beach.
+      </p>
+      <div className="skill">
+        <p style={{ backgroundColor: "blue" }}>React 💪</p>
+        <p style={{ backgroundColor: "orange" }}>HTML + CSS 💪</p>
+        <p style={{ backgroundColor: "yellow" }}>JavaScript 💪</p>
+        <p style={{ backgroundColor: "brown" }}>Svelte 👶</p>
+      </div>
+    </main>
+  );
+}
+
+const root = createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
