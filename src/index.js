aae190971b5f77f6ac866369180300d05b8b1823
obinna <temitopelaw1@gmail.com>
2025-06-10 15:34:06 +0100

    some react practice

--- src/index.js
+++ src/index.js
@@ -1,43 +1,105 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import React from "react";
 import "./index.css";
+
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
 
 function App() {
   return (
     <div className="card">
       <Avatar />
-      <Main />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
     </div>
   );
 }
 
 function Avatar() {
-  return <img src="https://8szvtk.csb.app/jonas.jpeg" />;
+  return (
+    <img
+      className="avatar"
+      src="https://8szvtk.csb.app/jonas.jpeg"
+      alt="Jonas Schmedtmann"
+    />
+  );
 }
 
-function Main() {
+function Intro() {
   return (
-    <main>
+    <div>
       <h1>Jonas Schmedtmann</h1>
       <p>
         Full-stack web developer and teacher at Udemy. When not coding or
         preparing a course, I like to play board games, to cook (and eat), or to
         just enjoy the Portuguese sun at the beach.
       </p>
-      <div className="skill">
-        <p style={{ backgroundColor: "blue" }}>React 💪</p>
-        <p style={{ backgroundColor: "orange" }}>HTML + CSS 💪</p>
-        <p style={{ backgroundColor: "yellow" }}>JavaScript 💪</p>
-        <p style={{ backgroundColor: "brown" }}>Svelte 👶</p>
-      </div>
-    </main>
+    </div>
   );
 }
 
-const root = createRoot(document.getElementById("root"));
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {skills.map((skill) => {
+        return (
+          <Skill skill={skill.skill} level={skill.level} color={skill.color} />
+        );
+      })}
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>
+        {props.level === "beginner" && "👶"}
+        {props.level === "intermediate" && "👍"}
+        {props.level === "advanced" && "💪"}
+      </span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
